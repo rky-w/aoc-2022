2c20ad1218e516533c502e953ff75dd765b6d607
rky-w <57967688+rky-w@users.noreply.github.com>
2022-12-02 00:07:30 +0000

    Adding code

--- aoc-2022-01.py
+++ aoc-2022-01.py
@@ -2,4 +2,16 @@
 from aocd.models import Puzzle
 
 puzzle = Puzzle(year=2022, day=1)
-puzzle_data = [int(val) for val in puzzle.input_data.splitlines()]
+
+cals = []
+tot = 0
+for val in puzzle.input_data.splitlines():
+    if val == '':
+        cals.append(tot)
+        tot = 0
+    else:
+        tot += int(val)
+
+max(cals)
+
+sum(sorted(cals, reverse=True)[:3])
