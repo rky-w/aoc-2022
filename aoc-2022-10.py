a99c8ad36613aaee4b261e451582cfd884fce115
rky-w <57967688+rky-w@users.noreply.github.com>
2023-11-13 00:46:10 +0000

    Completed day 10

--- aoc-2022-10.py
+++ aoc-2022-10.py
@@ -14,160 +14,6 @@ puzzle = Puzzle(year=2022, day=10)
 
 prog = puzzle.input_data.splitlines()
 
-prog = [
-    'noop',
-    'addx 3',
-    'addx -5',
-]
-
-prog = [
-    'addx 15',  
-    'addx -11', 
-    'addx 6',   
-    'addx -3',  
-    'addx 5',   
-    'addx -1',  
-    'addx -8',  
-    'addx 13',  
-    'addx 4',   
-    'noop', 
-    'addx -1',  
-    'addx 5',   
-    'addx -1',  
-    'addx 5',   
-    'addx -1',  
-    'addx 5',   
-    'addx -1',  
-    'addx 5',   
-    'addx -1',  
-    'addx -35', 
-    'addx 1',   
-    'addx 24',  
-    'addx -19', 
-    'addx 1',   
-    'addx 16',  
-    'addx -11', 
-    'noop', 
-    'noop', 
-    'addx 21',  
-    'addx -15', 
-    'noop', 
-    'noop', 
-    'addx -3',  
-    'addx 9',   
-    'addx 1',   
-    'addx -3',  
-    'addx 8',   
-    'addx 1',   
-    'addx 5',   
-    'noop', 
-    'noop', 
-    'noop', 
-    'noop', 
-    'noop', 
-    'addx -36', 
-    'noop', 
-    'addx 1',   
-    'addx 7',   
-    'noop', 
-    'noop', 
-    'noop', 
-    'addx 2',   
-    'addx 6',   
-    'noop', 
-    'noop', 
-    'noop', 
-    'noop', 
-    'noop', 
-    'addx 1',   
-    'noop', 
-    'noop', 
-    'addx 7',   
-    'addx 1',   
-    'noop', 
-    'addx -13', 
-    'addx 13',  
-    'addx 7',   
-    'noop', 
-    'addx 1',   
-    'addx -33', 
-    'noop', 
-    'noop', 
-    'noop', 
-    'addx 2',   
-    'noop', 
-    'noop', 
-    'noop', 
-    'addx 8',   
-    'noop', 
-    'addx -1',  
-    'addx 2',   
-    'addx 1',   
-    'noop', 
-    'addx 17',  
-    'addx -9',  
-    'addx 1',   
-    'addx 1',   
-    'addx -3',  
-    'addx 11',  
-    'noop', 
-    'noop', 
-    'addx 1',   
-    'noop', 
-    'addx 1',   
-    'noop', 
-    'noop', 
-    'addx -13', 
-    'addx -19', 
-    'addx 1',   
-    'addx 3',   
-    'addx 26',  
-    'addx -30', 
-    'addx 12',  
-    'addx -1',  
-    'addx 3',   
-    'addx 1',   
-    'noop', 
-    'noop', 
-    'noop', 
-    'addx -9',  
-    'addx 18',  
-    'addx 1',   
-    'addx 2',   
-    'noop', 
-    'noop', 
-    'addx 9',   
-    'noop', 
-    'noop', 
-    'noop', 
-    'addx -1',  
-    'addx 2',   
-    'addx -37', 
-    'addx 1',   
-    'addx 3',   
-    'noop', 
-    'addx 15',  
-    'addx -21', 
-    'addx 22',  
-    'addx -6',  
-    'addx 1',   
-    'noop', 
-    'addx 2',   
-    'addx 1',   
-    'noop', 
-    'addx -10', 
-    'noop', 
-    'noop', 
-    'addx 20',  
-    'addx 1',   
-    'addx 2',   
-    'addx 2',   
-    'addx -6',  
-    'addx -11', 
-    'noop', 
-    'noop', 
-    'noop', 
-]
 
 class Circuit():
     def __init__(self, prog):
@@ -176,19 +22,26 @@ class Circuit():
         self.X = 1
         self.addx_n = 0
         self.sig = 0
+        self.disp = ''
+
+    def draw(self):
+        if (self.X - 1) <= (
+            (self.cycle - (((self.cycle-1) // 40) * 40)) - 1
+            ) <= (self.X + 1):
+            self.disp += '#'
+        else:
+            self.disp += '.'
+        if self.cycle  == ((self.cycle // 40) * 40):
+            self.disp += '\n'
     
     def run(self):
         while c.prog:
             self.cycle += 1
             cmd = self.prog[0]
-            # print()
-            # print('cycle: ', self.cycle)
-            # print('cmd: ', cmd)
-            # print('X: ', self.X)
+            self.draw()
+
             if (self.cycle  == ((self.cycle - 20) // 40) * 40 + 20):
                 self.sig += self.cycle * self.X
-            if self.cycle >= 220:
-                break
             if cmd == 'noop':
                 self.prog.pop(0)
             else:
@@ -206,3 +59,5 @@ c.run()
 # Pt 1 answer
 print(c.sig)
 
+# Pt 2
+print(c.disp)
